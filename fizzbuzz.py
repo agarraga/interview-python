b8ee448e7345628474a4bb13f31717410a0160bf
agarrigu <agarrigu@protonmail.com>
2022-09-09 02:20:26 +0200

    fizzbuzz() now takes two arrays

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,54 @@
 import sys
 
-def fizzbuzz(n: int) -> list:
+
+class Pair:
+    def __init__(self, number: int, word: str):
+        self.number = number
+        self.word = word
+
+
+def fizzbuzz(numbers: list, words: list, n: int) -> list:
     if n < 1:
         raise ValueError("Invalid number: n must be bigger or equal to 1")
+    bigger_size = 0
+    words_size = len(words)
+    numbers_size = len(numbers)
+    if not all(isinstance(e, int) for e in numbers):
+        raise TypeError("numbers list must only contain int")
+    if not all(isinstance(e, str) for e in words):
+        raise TypeError("words list must only contain str")
+
+    if len(numbers) > len(words):
+        bigger_size = numbers_size
+        words = fill_words(words, bigger_size) 
+    elif len(numbers) < len(words):
+        bigger_size = words_size 
+        words = fill_words(words, bigger_size) 
+
     l = []
-    for i in range(1, n + 1):
+    pairs = [Pair(pair[0], pair[1]) for pair in zip(numbers, words)]
+    for i in range(n):
         output = ""
-        if i % 3 == 0: output += "Fizz" 
-        if i % 5 == 0: output += "Buzz"
-        if not output: output = str(i)
-        l.append(output)
+        for pair in pairs:
+            if i % pair.number == 0:
+                output += pair.word + " "
+        if output: l.append(output.rstrip())
+        else: l.append(str(i))
+
     return l
+
+
+def fill_words(words: list, total: int) -> list:
+    if not all(isinstance(e, str) for e in words):
+        raise TypeError("words list must only contain str")
+    return words
+
+
+def fill_numbers(numbers: list, total: int) -> list:
+    if not all(isinstance(e, int) for e in numbers):
+        raise TypeError("numbers list must only contain int")
+    return numbers
+
 
 def infinifizzbuzz():
     i = 1
@@ -22,15 +60,17 @@ def infinifizzbuzz():
         print(output)
         i += 1
 
+
 if __name__ == '__main__':
-    if (sys.argv[1:]) == "PRESS THE RED BUTTON":
+    if len(sys.argv) == 1:
+        print(*fizzbuzz([3, 5], ["Fizz", "Buzz"], 100), sep = '\n')
+        sys.exit()
+    elif (sys.argv[1:]) == "PRESS THE RED BUTTON":
         infinifizzbuzz()
-    if sys.argv:
-        try:
-            n = int(sys.argv[1])
-            print(*fizzbuzz(n), sep = '\n')
-        except ValueError:
-            print("Argument must be a positive integer.")
-            sys.exit(1)
-    else:
-        print(*fizzbuzz(100), sep = '\n')
+        sys.exit()
+    try:
+        n = int(sys.argv[1])
+        print(*fizzbuzz([3, 5], ["Fizz", "Buzz"], n), sep = '\n')
+    except ValueError:
+        print("Argument must be a positive integer.")
+        sys.exit(1)
